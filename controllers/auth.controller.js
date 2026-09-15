1d5e9d06df158bc9908a988bc5a4be829b3daeaf
Jeff Yankson <jjyankson19@gmail.com>
2026-09-15 16:41:25 +0000

    feat: create user login controller, add password matching methods, fix bugs and test

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,3 +19,32 @@ export const register = asyncHandler(async (req, res, next) => {
     token,
   });
 });
+
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(
+      new CustomErrorHandlerAPI('Please enter a valid email and password', 401),
+    );
+  }
+
+  // Check if user exists.
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new CustomErrorHandlerAPI('User not found', 401));
+  }
+
+  const isTrue = await user.matchPassword(password);
+
+  if (!isTrue) {
+    return next(new CustomErrorHandlerAPI('Invalid password', 401));
+  }
+
+  const token = user.getSignedJwtToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
--- models/user.model.js
+++ models/user.model.js
@@ -64,4 +64,8 @@ UserSchema.methods.getSignedJwtToken = function () {
   });
 };
 
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 export default mongoose.model('User', UserSchema);
