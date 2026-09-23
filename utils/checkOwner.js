5085917f9227166dbbc11b4d3d87be27ba279ce5
Jeff Yankson <jjyankson19@gmail.com>
2026-09-23 16:59:27 +0000

    feat: add checkOwner utility for permission validation

--- utils/checkOwner.js
+++ utils/checkOwner.js
@@ -1,14 +1,14 @@
 import CustomErrorHandlerAPI from '../helpers/customErrorHandlerAPI.js';
 
-const checkIfBootcampExists = (instance, req, statusCode, next) => {
+const checkOwner = (instance, req, statusCode, next) => {
   if (instance.user.toString() !== req.user.id && req.user.role !== 'admin') {
     return next(
       new CustomErrorHandlerAPI(
-        `You do not have the right permissions to delete  bootcamp`,
-        statusCode,
+        `You do not have the right permissions`,
+        statusCode, 
       ),
     );
   }
 };
 
-export default checkIfBootcampExists;
+export default checkOwner;
